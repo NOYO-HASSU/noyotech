b2a046c3ecde54931a05c9b3e424dd3f191ec3de
NOYO-HASSU <135537044+NOYO-HASSU@users.noreply.github.com>
2025-03-08 19:57:14 +0000

    updated

--- js/magnific-popup-options.js
+++ js/magnific-popup-options.js
@@ -1,32 +1,19 @@
 $(document).ready(function() {
-  // MagnificPopup
-  var magnifPopup = function() {
-    $('.popup-image').magnificPopup({
+  // MagnificPopup Initialization
+  $('.popup-image').magnificPopup({
       type: 'image',
       removalDelay: 300,
       mainClass: 'mfp-with-zoom',
-      gallery:{
-        enabled:true
+      gallery: {
+          enabled: true // Enables gallery mode for multiple images
       },
       zoom: {
-        enabled: true, // By default it's false, so don't forget to enable it
-
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
-
-        // The "opener" function should return the element from which popup will be zoomed in
-        // and to which popup will be scaled down
-        // By defailt it looks for an image tag:
-        opener: function(openerElement) {
-        // openerElement is the element on which popup was initialized, in this case its <a> tag
-        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-        return openerElement.is('img') ? openerElement : openerElement.find('img');
-        }
+          enabled: true,
+          duration: 300,
+          easing: 'ease-in-out',
+          opener: function(openerElement) {
+              return openerElement.is('a') ? openerElement.find('img') : openerElement;
+          }
       }
-    });
-  };
-  
-  // Call the functions 
-  magnifPopup();
-
+  });
 });
